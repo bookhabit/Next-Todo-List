8c787725dea62c13755dd013e6e2791d06d398f9
bookhabit <qnrgoqlt@naver.com>
2023-01-07 22:21:39 +0900

    스타일드 컴포넌트 설치 및 글로벌 설정

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import {NextPage} from "next"
+import styled from "styled-components";
 
-const index = () => {
+const Container = styled.div`
+    font-style:italic;
+    color:red;
+    padding:20px;
+`
+
+const index:NextPage = ()=>{
     return (
-        <div>
-            hello Typescript
-        </div>
-    );
-};
+        <Container>
+            <h1>hello Typescript</h1>
+            <h2>hello Typescript</h2>
+            <p>hello Typescript</p>
+            <ul>
+                <li>hello Typescript</li>
+            </ul>
+            <a>hello Typescript</a>
+            <span>hello Typescript</span>
+        </Container>
+    )
+}
 
 export default index;
