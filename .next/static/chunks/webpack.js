44d560dff1ddb1053aefbd25ddddbaf8842006e4
bookhabit <qnrgoqlt@naver.com>
2023-01-15 15:03:05 +0900

    리덕스 스토어 생성하기

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0d167fd6e36b20ec"; }
+/******/ 		__webpack_require__.h = function() { return "285a80a08b233663"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import GlobalStyle from "../styles/GlobalStyle"
 import Header from "../components/Header"
 import React from "react"
 import Footer from "../components/Footer"
+import { wrapper } from "../store"
 
 const app = ({Component,pageProps}:AppProps)=>{
     return(
@@ -15,4 +16,4 @@ const app = ({Component,pageProps}:AppProps)=>{
     )
 }
 
-export default app;
+export default wrapper.withRedux(app);
